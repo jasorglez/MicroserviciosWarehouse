f2da2706280a24e8b665aa55aa36fbcd8b468fa4
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2024-10-06 08:43:00 -0600

    2 da vers ion para  repository Warehouse for Microservice Warehouse

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using Warehouse;
 using Warehouse.Service;
 
@@ -8,20 +12,53 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigin",
+    options.AddPolicy("AllowWarehouseOrigin",
         builder =>
         {
             builder
-                .WithOrigins("http://localhost:4200", "https://be-app-five.vercel.app", "https://localhost:7089") // Reemplaza esto con el origen de tu aplicación Angular
+                .WithOrigins("http://localhost:4200", "https://be-app-five.vercel.app", "http://localhost:8100",
+                             "https://localhost:7089", "https://smp25-beta.netlify.app") // Reemplaza esto con el origen de tu aplicacin Angular
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials();
         });
 });
 
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1.5", new OpenApiInfo { Title = "Microservicio Warehouse", Version = "v1.5 Mod. 06-10-24 13:58" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Ejemplo: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
 builder.Services.AddSignalR();
 builder.Services.AddHttpClient();
 builder.Services.AddLogging();
@@ -34,10 +71,23 @@ builder.Services.AddDbContext<DbWarehouseContext>(options =>
 //WareHouse BD and Services
 builder.Services.AddScoped<IWarehouseService, WarehouseService>();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                builder.Configuration["Jwt:Key"] ?? throw new InvalidOperationException("La clave JWT no está configurada")))
+        };
+
+    });
 
 var app = builder.Build();
 
@@ -45,10 +95,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1.5/swagger.json", "Microservicio Warehouse V1.5");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
+
+
+app.UseCors("AllowWarehouseOrigin");
 
 app.UseAuthorization();
 
