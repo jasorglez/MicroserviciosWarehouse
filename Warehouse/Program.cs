24e546d727365b23b40011a4054dbbcb1507d3df
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-04-21 15:51:48 -0600

    Updated changes in Program.cs

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
             builder
 
                 .WithOrigins("http://localhost:4200", "https://be-app-five.vercel.app", "http://localhost:8100",
-                             "https://localhost:7089", "https://biapp.com.mx", "https://localhost:4200", "https://www.bi2.com.mx") // Reemplaza esto con el origen de tu aplicacin Angular
+                             "https://localhost:7089", "https://biapp.com.mx", "https://localhost:4200", "https://www.bi2.com.mx", "https://bi2.com.mx") // Reemplaza esto con el origen de tu aplicacin Angular
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials();
@@ -39,7 +39,7 @@ builder.Services.AddSwaggerGen(c =>
  /*     c.SwaggerDoc("v1.99", new OpenApiInfo { Title = "Microservicio Warehouse", Version = "v1.99 Mod. 14-11-24 11:12 20.221.74.88" });
  */
 
-    c.SwaggerDoc("v2.25", new OpenApiInfo { Title = "Microservicio Warehouse", Version = "v2.25 Mod. 2025-04-21 15:51, BSKG, Server 198.71.49.16" }); 
+    c.SwaggerDoc("v2.25", new OpenApiInfo { Title = "Microservicio Warehouse With CORS", Version = "v2.25 Mod. 2025-04-21 15:51, BSKG, Server 198.71.49.16" }); 
 
   
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
