932ef1684641111620dc99a16c95c529cbbc4fa5
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-08-26 13:00:39 -0600

    update Materials

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -37,9 +37,8 @@ builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.Re
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-;
 
-    c.SwaggerDoc("v2.26", new OpenApiInfo { Title = "Microservicio Warehouse", Version = "v2.33 Mod. 25-08-2025 10:48, SBKG, Server 66.179.240.10" }); 
+    c.SwaggerDoc("v2.26", new OpenApiInfo { Title = "Microservicio Warehouse", Version = "v2.33 Mod. 25-08-2025 12:48, SBKG, Server 66.179.240.10" }); 
 
   
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
