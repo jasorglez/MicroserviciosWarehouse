716197a32bdaf2ad683c27e120ca3dc7dc7d5424
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-01-12 08:36:18 -0600

    update Materials in requisition details

--- Warehouse/Models/Detailsreqoc.cs
+++ Warehouse/Models/Detailsreqoc.cs
@@ -1,7 +1,6 @@
-﻿
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Warehouse.Models
 {
@@ -12,40 +11,66 @@ namespace Warehouse.Models
         [Column("id")]
         public int Id { get; set; }
 
-        
         [Column("id_movement")]
         public int IdMovement { get; set; }
 
-        
         [Column("id_supplie")]
         public int IdSupplie { get; set; }
 
         [Column("id_provider")]
-        public int IdProvider { get; set; }
+        public int? IdProvider { get; set; }
 
-        [Required]
-        [Column("dateuse", TypeName = "date")]
-        public DateTime Dateuse { get; set; }
+        [Column("name_provider")]
+        public string? NameProvider { get; set; }
 
-        [Required]
-        [Column("quantity", TypeName = "DECIMAL(16,2)")]
+        [Column("quantity", TypeName = "decimal(16,2)")]
         public decimal Quantity { get; set; }
 
-        [Required]
-        [Column("price", TypeName = "DECIMAL(16,2)")]
-        public decimal Price { get; set; }               
+        [Column("price", TypeName = "decimal(16,2)")]
+        public decimal Price { get; set; }
 
-        // Propiedad de solo lectura para calcular el total en el código si es necesario
+        // Computed column in SQL
         [NotMapped]
         public decimal Total => Quantity * Price;
-    
 
-        [Column("type", TypeName = "VARCHAR")]
+        [Column("dateuse", TypeName = "date")]
+        public DateTime? Dateuse { get; set; }
+
+        [Column("type")]
         [StringLength(6)]
         public string? Type { get; set; }
 
-        [Column("comment", TypeName = "VARCHAR")]
+        [Column("recurrent")]
+        [StringLength(10)]
+        public string Recurrent { get; set; } = "Recurrente";
+
+        [Column("namearticle")]
+        [StringLength(50)]
+        public string? NameArticle { get; set; }
+
+        [Column("numarticle")]
         [StringLength(20)]
+        public string? NumArticle { get; set; }
+
+        [Column("intorext")]
+        [StringLength(10)]
+        public string IntOrExt { get; set; } = "Interno";
+
+        [Column("provint")]
+        [StringLength(50)]
+        public string? ProvInt { get; set; }
+
+        [Column("typepriority")]
+        [StringLength(8)]
+        public string? TypePriority { get; set; }
+
+        [Column("pedimento")]
+        public bool? Pedimento { get; set; }
+
+        [Column("pedimentonum")]
+        public int PedimentoNum { get; set; }
+
+        [Column("comment")]
         public string? Comment { get; set; }
 
         [Column("active")]
