f0c7b92be1c5517451fff35b6a89737e25ad6c7a
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-04-01 14:43:29 -0600

    update Service/ProveedorXTablaService.cs

--- Warehouse/Service/ProveedorXTablaService.cs
+++ Warehouse/Service/ProveedorXTablaService.cs
@@ -82,6 +82,7 @@ namespace Warehouse.Service
             existing.Campo6 = proveedor.Campo6;
             existing.Campo7 = proveedor.Campo7;
             existing.Campo8 = proveedor.Campo8;
+            existing.Campo11 = proveedor.Campo11;
             //existing.Type = proveedor.Type;
             existing.Vigente = proveedor.Vigente;
             existing.Principal = proveedor.Principal;
