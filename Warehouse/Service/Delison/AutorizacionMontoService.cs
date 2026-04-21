ec7af9a934e5d630fd988986de999debd3c755ad
David Kantun <dkantun@gmail.com>
2026-04-20 18:39:26 -0600

    Mas cambios a autorizacion monto

--- Warehouse/Service/Delison/AutorizacionMontoService.cs
+++ Warehouse/Service/Delison/AutorizacionMontoService.cs
@@ -24,10 +24,26 @@ namespace Warehouse.Service.Delison
 
         public async Task<List<AutorizacionMonto>> GetByCompany(int idCompany)
         {
-            return await _context.AutorizacionMontos
+            var existing = await _context.AutorizacionMontos
                 .Where(a => a.IdCompany == idCompany && a.Active)
                 .OrderBy(a => a.Nivel)
                 .ToListAsync();
+
+            if (existing.Count > 0)
+                return existing;
+
+            var defaults = new List<AutorizacionMonto>
+            {
+                new() { IdCompany = idCompany, Nivel = 1, MontoMin = 1m,       MontoMax = 5000m,   Descripcion = "Nivel 1: $1 a $5,000" },
+                new() { IdCompany = idCompany, Nivel = 2, MontoMin = 5001m,    MontoMax = 25000m,  Descripcion = "Nivel 2: $5,001 a $25,000" },
+                new() { IdCompany = idCompany, Nivel = 3, MontoMin = 25001m,   MontoMax = 100000m, Descripcion = "Nivel 3: $25,001 a $100,000" },
+                new() { IdCompany = idCompany, Nivel = 4, MontoMin = 100001m,  MontoMax = null,    Descripcion = "Nivel 4: $100,001 en adelante" }
+            };
+
+            _context.AutorizacionMontos.AddRange(defaults);
+            await _context.SaveChangesAsync();
+
+            return defaults;
         }
 
         public async Task<AutorizacionMonto?> GetById(int id)
