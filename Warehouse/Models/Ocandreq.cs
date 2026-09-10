535265e33f18428257001fc318df56bbd8a8f1aa
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-09-10 07:13:09 -0400

    feat: persist warehouse on requisitions and orders

--- Warehouse/Models/Ocandreq.cs
+++ Warehouse/Models/Ocandreq.cs
@@ -38,6 +38,9 @@ namespace Warehouse.Models
         [Column("id_provider")]
         public int? IdProvider { get; set; } = 0;
 
+        [Column("id_warehouse")]
+        public int? IdWarehouse { get; set; }
+
         [Required]
         [Column("id_departament")]
         public int IdDepartament { get; set; } = 0;
